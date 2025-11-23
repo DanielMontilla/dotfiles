04a5152eaf6d2199b79dcd7fac9f21b63832b4ea
Daniel Montilla <daniel@montilla.dev>
2025-11-23 19:55:59 -0400

    chore: checkpoint

--- flake.nix
+++ flake.nix
@@ -3,19 +3,22 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixgl.url = "github:nix-community/nixGL";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, nixgl }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
         inherit system;
         config.allowUnfree = true;
+        # overlays = [ nixgl.overlay ];
       };
       my-packages = [
         # System
         pkgs.git
         pkgs.dotbot
+        pkgs.glibcLocales 
 
         # Fonts
         pkgs.nerd-fonts.jetbrains-mono
@@ -35,10 +38,15 @@
         pkgs.brave
         pkgs.code-cursor
         pkgs.slack
+        # pkgs.ghostty
+        # (pkgs.writeShellScriptBin "ghostty" ''
+        #   exec ${(pkgs.nixgl.nvidiaPackages { version = "580.82.09"; sha256 = null; }).nixGLNvidia}/bin/nixGLNvidia ${pkgs.ghostty}/bin/ghostty "$@"
+        # '')
 
         # Hyprland
         pkgs.wayland
-        pkgs.waybar
+        pkgs.quickshell
+        # pkgs.waybar
       ];
     in {
       packages.${system}.default = pkgs.buildEnv {
--- shared/fish/config.fish
+++ shared/fish/config.fish
@@ -1,5 +1,15 @@
 set fish_greeting
 
+# Set LOCALE_ARCHIVE for Nix packages to use UTF-8 locales from glibcLocales
+# Checks user profile first, then falls back to system default profile
+if test -f ~/.nix-profile/lib/locale/locale-archive
+  set -gx LOCALE_ARCHIVE ~/.nix-profile/lib/locale/locale-archive
+else
+  if test -f /nix/var/nix/profiles/default/lib/locale/locale-archive
+    set -gx LOCALE_ARCHIVE /nix/var/nix/profiles/default/lib/locale/locale-archive
+  end
+end
+
 # Starship setup
 starship init fish | source
 :
