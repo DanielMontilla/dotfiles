cb6d306fc39cd5fadf337a81afa5f2fe7d821186
Daniel Montilla <daniel@montilla.dev>
2026-01-13 18:21:30 -0400

    feat: remove repo zellij layout, add function alias

--- shared/fish/config.fish
+++ shared/fish/config.fish
@@ -5,5 +5,9 @@ function ls --wraps eza --description 'alias ls=eza'
   eza --icons --group-directories-first $argv
 end
 
+function zj --wraps zellij --description 'alias zj=zellij with optional layout'
+  zellij $argv
+end
+
 # Starship setup
 starship init fish | source
