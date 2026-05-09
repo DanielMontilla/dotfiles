fd860c3bebdcd4a174577cf229b79a2a70701857
Daniel Montilla <daniel@montilla.dev>
2026-05-09 15:09:03 -0400

    feat(nixos): update framework system configuration

--- nixos/hosts/framework/networking.nix
+++ nixos/hosts/framework/networking.nix
@@ -4,9 +4,41 @@
   networking.hostName = "nixos";
   networking.networkmanager.enable = true;
 
-  # OpenVPN support for NetworkManager
   networking.networkmanager.plugins = with pkgs; [
     networkmanager-openvpn
   ];
+
+  networking.wg-quick.interfaces.wg0 = {
+    address = [ "10.14.0.2/16" ];
+    listenPort = 51820;
+    privateKeyFile = "/etc/nixos/secrets/surfshark.key";
+    table = "off";
+
+    postUp = let
+      ip = "${pkgs.iproute2}/bin/ip";
+      sysctl = "${pkgs.procps}/bin/sysctl";
+    in ''
+      ${ip} route add default dev wg0 table 51820
+      ${ip} rule add from 10.14.0.2 table 51820 priority 100
+      ${sysctl} -w net.ipv4.conf.wg0.rp_filter=2
+    '';
+
+    preDown = let ip = "${pkgs.iproute2}/bin/ip"; in ''
+      ${ip} rule del from 10.14.0.2 table 51820 priority 100
+      ${ip} route del default dev wg0 table 51820
+    '';
+
+    peers = [
+      {
+        publicKey = "lLqqxZuCTtIpBjgZJYWzPQn/7st24iVpJN+/xS7jogs=";
+        allowedIPs = [ "0.0.0.0/0" ];
+        endpoint = "co-bog.prod.surfshark.com:51820";
+        persistentKeepalive = 25;
+      }
+    ];
+  };
+
+  networking.firewall.allowedUDPPorts = [ 51820 ];
+  networking.firewall.trustedInterfaces = [ "wg0" ];
 }
 
--- nixos/hosts/framework/packages.nix
+++ nixos/hosts/framework/packages.nix
@@ -1,9 +1,45 @@
 { config, pkgs, inputs, ... }:
 
+let
+  cursor-src = pkgs.fetchurl {
+    url = "https://api2.cursor.sh/updates/download/golden/linux-x64/cursor/2.5";
+    sha256 = "csvj0THOX/RPi7Docv033mi8DlEOEdnjFIQ8jL7HPO8=";
+  };
+  cursor-appimage = pkgs.writeScriptBin "cursor" ''
+    #!${pkgs.runtimeShell}
+    nohup ${pkgs.appimage-run}/bin/appimage-run ${cursor-src} "$@" </dev/null >/dev/null 2>&1 &
+  '';
+in
+
 {
+  services.flatpak = {
+    enable = true;
+    remotes = [
+      { name = "flathub"; location = "https://dl.flathub.org/repo/flathub.flatpakrepo"; }
+    ];
+    packages = [
+      "com.usebruno.Bruno"
+      # "dev.zed.Zed"  # using nixpkgs version instead
+      "org.pgadmin.pgadmin4"
+    ];
+  };
+
   programs.hyprland.enable = true;
 
   nixpkgs.config.allowUnfree = true;
+
+  services.cloudflared = {
+    enable = true;
+    tunnels = {
+      "06101592-5b5b-4d9a-a00a-7db47d0ca40e" = {
+        credentialsFile = "/var/lib/cloudflared/tunnel-creds.json";
+        ingress = {
+          "montilla.pagoasap.com" = "http://localhost:4318";
+        };
+        default = "http_status:404";
+      };
+    };
+  };
 
   services.upower.enable = true;
 
@@ -17,6 +53,7 @@
     extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
   };
 
+  environment.localBinInPath = true;
   environment.sessionVariables = {
     GTK_THEME = "Adwaita:dark";
   };
@@ -24,14 +61,15 @@
   environment.systemPackages = with pkgs; [
     git
     neovim
+    zed-editor
     dotbot
     ghostty
     kitty
+    wl-clipboard
     fish
     brave
     firefox
     spotify
-    code-cursor
     bitwarden-desktop
     starship
     quickshell
@@ -46,14 +84,20 @@
     lazygit
     zellij
     networkmanagerapplet
-    bruno
     fastfetch
     lm_sensors
+    gammastep
+    qbittorrent
+    vlc
+    cursor-appimage
+    glances
+    btop
   ];
+
+  programs.fish.enable = true;
 
   programs.nix-ld.enable = true;
   programs.nix-ld.libraries = with pkgs; [
     tree-sitter
   ];
 }
-
