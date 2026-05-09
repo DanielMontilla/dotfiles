5b5df1d41feaf051da75971adcb1078d2d41a40e
Daniel Montilla <daniel@montilla.dev>
2026-05-09 15:09:08 -0400

    feat(tools): update shell and editor configs

--- shared/fish/config.fish
+++ shared/fish/config.fish
@@ -1,13 +1,8 @@
-set fish_greeting
-
-# Functions
-function ls --wraps eza --description 'alias ls=eza'
-  eza --icons --group-directories-first $argv
+# use nixpkgs zed (not flatpak)
+function zed --description 'Zed editor'
+    command zeditor $argv & 
 end
 
-function zj --wraps zellij --description 'alias zj=zellij with optional layout'
-  zellij $argv
-end
-
-# Starship setup
 starship init fish | source
+
+fish_add_path $HOME/.local/bin
