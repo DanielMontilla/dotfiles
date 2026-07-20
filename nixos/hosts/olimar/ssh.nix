0125e1c2f0140824e62180c1b75ec636cbd31649
Daniel Montilla <daniel@montilla.dev>
2026-07-19 21:22:15 -0400

    fix(ssh): rotate olimar authorized_keys to louie's new ed25519 key

--- nixos/hosts/olimar/ssh.nix
+++ nixos/hosts/olimar/ssh.nix
@@ -15,7 +15,7 @@
 
   # olimar trusts louie's key (per-machine: only peer public keys go here)
   users.users.daniel.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE8OTlg/sl4xMJJZUJBEgFv6cJi3r2NMoATgIiLvdf9+ daniel@louie"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN/nUf7c2Sekdov5CZspz7GslJacskM2MA8mrnwKdbhO daniel@louie"
   ];
 
   # Tailscale mesh VPN for internet reachability (free personal plan)
