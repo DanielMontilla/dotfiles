94d679df46afc5e30e5175a39c74aad67e0c5446
Daniel Montilla <daniel@montilla.dev>
2026-09-13 18:57:21 -0400

    feat(olimar): keep high-quality A2DP bluetooth audio

--- nixos/hosts/olimar/bluetooth.nix
+++ nixos/hosts/olimar/bluetooth.nix
@@ -8,10 +8,29 @@
       General = {
         Experimental = true;
         FastConnectable = true;
+        Enable = "Source,Sink,Media,Socket";
+        MultiProfile = "multiple";
       };
       Policy = {
         AutoEnable = true;
       };
+    };
+  };
+
+  # Keep high-quality A2DP as default; stop WirePlumber from
+  # auto-dropping to mono HFP whenever an app opens a mic.
+  services.pipewire.wireplumber.extraConfig."10-bluez" = {
+    "monitor.bluez.properties" = {
+      "bluez5.enable-sbc-xq" = true;
+      "bluez5.enable-msbc" = true;
+      "bluez5.enable-hw-volume" = true;
+      "bluez5.roles" = [ "a2dp_sink" "a2dp_source" "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
+    };
+  };
+
+  services.pipewire.wireplumber.extraConfig."11-bluetooth-policy" = {
+    "wireplumber.settings" = {
+      "bluetooth.autoswitch-to-headset-profile" = false;
     };
   };
 
@@ -21,5 +40,7 @@
   environment.systemPackages = with pkgs; [
     bluetui
     bluez
+    pavucontrol
+    pwvucontrol
   ];
 }
